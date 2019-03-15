8337b0101b27d895742a8484241b3b87250c1c6c
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-03-15 00:52:00 -0300

    Gradle build and auth system

---
+++ back/hive/naming-server/src/test/java/hive/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package hive.namingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
