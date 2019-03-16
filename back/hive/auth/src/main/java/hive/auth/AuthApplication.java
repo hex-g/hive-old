84585d0fa9add5b448bce6881def28caa0f97f72
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-03-16 15:11:05 -0300

    Create RestTemplate

--- back/hive/auth/src/main/java/hive/auth/AuthApplication.java
+++ back/hive/auth/src/main/java/hive/auth/AuthApplication.java
@@ -2,7 +2,11 @@ package hive.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,13 @@ public class AuthApplication {
     SpringApplication.run(AuthApplication.class, args);
   }
 
+}
+
+@Configuration
+class RestTemplateConfig {
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
--- back/hive/auth/src/main/java/hive/auth/security/UserDetailsServiceImpl.java
+++ back/hive/auth/src/main/java/hive/auth/security/UserDetailsServiceImpl.java
@@ -12,15 +12,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service   // It has to be annotated with @Service.
 public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  private RestTemplate restTemplate;
 
   @Autowired
   private BCryptPasswordEncoder encoder;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    final Object test = restTemplate.getForObject("http://repository-service", Object.class);
 
     // hard coding the users. All passwords must be encoded.
     final List<AppUser> users = Arrays.asList(
@@ -28,10 +32,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         new AppUser(2, "admin", encoder.encode("12345"), "ADMIN")
     );
 
-
     for (AppUser appUser : users) {
       if (appUser.getUsername().equals(username)) {
-
         // Remember that Spring needs roles to be in this format: "ROLE_" + userRole (i.e. "ROLE_ADMIN")
         // So, we need to set it to that format, so we can verify and compare roles (i.e. hasRole("ADMIN")).
         List<GrantedAuthority> grantedAuthorities = AuthorityUtils
